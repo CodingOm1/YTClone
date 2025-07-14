25099f5dad64487a648ba6d537b4f5acb4421753
Om Waghmare <164727053+CodingOm1@users.noreply.github.com>
2025-07-14 15:44:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ mongoose.connect('mongodb+srv://codewithom405:RuA6ZF88VzzLKOW0@cluster0.v4ics.mo
 
 // Middleware
 app.use(cors({
-  origin: 'https://omtube.vercel.app'
+  origin: 'https://omtube.vercel.app/'
 }));
 
 app.use(express.json());
